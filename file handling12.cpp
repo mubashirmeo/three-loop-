1b377c1c5e723d24b354facdf272cad98fea07ce
mubashirmeo <121413909+mubashirmeo@users.noreply.github.com>
2023-01-08 22:44:15 +0500

    Add files via upload

---
+++ file handling12.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+{
+  ofstream Mydacoment("informa.txt");
+   Mydacoment<<" Ali";
+   Mydacoment<<"03226117835";
+   Mydacoment<<"matric";
+  
+      Mydacoment.close(); 
+}
+{
+  ofstream Mydacoment("information.txt");
+   Mydacoment<<"Mubashir Ali"<<endl;
+  Mydacoment<<"03226117835"<<endl;
+   Mydacoment<<"science"<<endl;
+      Mydacoment.close(); 
+}
+string myMydacoment;
+ ifstream MyOnlydacoment("information.txt");
+  while(getline(MyOnlydacoment,myMydacoment)){
+  	cout<<myMydacoment<<endl;}
+  
+{ ofstream Mydacoment("information.txt");
+ for(i=0;i< Mydacoment;i++)
+   if( Mydacoment[i]==' ')
+   words=words +1
+   cout<<"count total word in 1st file ="<<words;}
+   {
+ifstream MyOnlydacoment("information.txt");
+for(i=0;i< Mydacoment;i++)
+   if( Mydacoment[i]==' ')
+   words=words +1
+   cout<<"count total word in 1st file ="<<words;}
+
+cout<<" total similar word =";
+cout<< fstream ;
+	MyOnlydacoment.close(); }
+}
